bde4a7556129cfd83a017f165fa15051f2f73306
Slamkas <seslamka@yahoo.com>
2014-11-19 14:57:02 -0700

    Create uart.c

---
+++ uart.c
@@ -0,0 +1,11 @@
+//
+//  uart.c
+//
+//	Proj05
+//  Lab Partners: Sean Slamka, Aydin Balci
+//  Email: sslamka@asu.edu, abalci@asu.edu
+//  CSE325 Embedded Microprocessor Systems
+//  Fall 2014
+//
+
+#include "uart.h"
