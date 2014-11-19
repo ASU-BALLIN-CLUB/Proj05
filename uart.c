54d4948b9f8c8587f280f066410750688fdc6f70
Slamkas <seslamka@yahoo.com>
2014-11-19 15:50:22 -0700

    Update uart.c

--- uart.c
+++ uart.c
@@ -9,3 +9,18 @@
 //
 
 #include "uart.h"
+
+
+void uart_init()
+{
+	MCF_UART_UMR11 = 0x0;
+	MCF_UART_UMR11 = 0b00001011;
+	MCF_UART_UMR21 = 0x0;
+	MCF_UART_UMR21 = 0b00000001;
+
+}
+
+void uart_begin()
+{
+
+}
