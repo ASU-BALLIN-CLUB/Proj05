a527923b7642be22dfa3a066bb0cd5797dcc6ee7
Slamkas <seslamka@yahoo.com>
2014-11-19 14:57:17 -0700

    Create uart.h

---
+++ uart.h
@@ -0,0 +1,28 @@
+//
+//  uart.h
+//
+//	Proj02
+//  Lab Partners: Sean Slamka, Aydin Balci
+//  Email: sslamka@asu.edu, abalci@asu.edu
+//  CSE325 Embedded Microprocessor Systems
+//  Fall 2014
+//
+
+#ifndef ____uart__
+#define ____uart__
+
+
+#include"gpio.h"
+#include"global.h"
+#include"int.h"
+#include"uc_dipsw.h"
+#include"gpt.h"
+#include"oct_rgb_led.h"
+#include"pit.h"
+#include"pwm.h"
+#include"uc_pushb.h"
+#include"uc_led.h"
+
+
+
+#endif
